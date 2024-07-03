3bfc36b05ad0388d311b2009baf95158d602f10a
mb8144 <you@example.com>
2024-07-03 11:40:43 +0200

    added tailwind and daisyui

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,9 +1,11 @@
-import styles from "./index.module.css"
+import styles from "./index.module.css";
 
 export default function IndexPage() {
-    return (
-        <div className={styles.posts}>
-            <h1>Welcome to my blog!</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Welcome to my blog!</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p className="text-xl text-red-500">sdf</p>
+    </div>
+  );
 }

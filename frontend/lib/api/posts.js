933cf35043ca0b41b8e3e788c5ddc6881d767429
mb8144 <you@example.com>
2024-07-03 12:07:46 +0200

    CRUD operations

--- frontend/lib/api/posts.js
+++ frontend/lib/api/posts.js
@@ -1,2 +1,64 @@
-const URL = process.env.NEXT_PUBLIC_API_URL
+const URL = process.env.NEXT_PUBLIC_API_URL;
 
+export async function getAllPosts() {
+  const response = await fetch(`${URL}/posts`);
+  if (!response.ok) {
+    return Promise.reject(response.statusText);
+  }
+  const data = await response.json();
+  return data;
+}
+
+export async function getPostById(id) {
+  const response = await fetch(`${URL}/posts/${id}`);
+  if (!response.ok) {
+    return Promise.reject(response.statusText);
+  }
+  const data = await response.json();
+  return data;
+}
+
+export async function createPost(post) {
+  const response = await fetch(`${URL}/posts`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(post),
+  });
+  if (!response.ok) {
+    return Promise.reject(response.statusText);
+  }
+  const data = await response.json();
+  return data;
+}
+
+export async function updatePost(post) {
+  const response = await fetch(`${URL}/posts/${post.id}`, {
+    method: "PUT",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(post),
+  });
+  if (!response.ok) {
+    return Promise.reject(response.statusText);
+  }
+  const data = await response.json();
+  return data;
+}
+
+export async function deletePost(post) {
+    const response = await fetch(`${URL}/posts/${post.id}`, {
+      method: "DELETE",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(post),
+    });
+    if (!response.ok) {
+      return Promise.reject(response.statusText);
+    }
+    const data = await response.json();
+    return data;
+  }
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,5 +1,3 @@
-import styles from "./index.module.css";
-
 export default function IndexPage() {
   return (
     <div>
